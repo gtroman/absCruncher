849a8e2326f40200c84910556fb5626e5f9e8cd1
Glenn Roman <create@glennroman.com>
2015-12-10 13:59:38 -0800

    improved copy paste function, fixed double element creation issue

--- js/absCruncher.js
+++ js/absCruncher.js
@@ -1,8 +1,25 @@
 //Run plugin on relative container
 $.fn.absCruncher = function() {
 
-	//Calulate Positions
-	function calcOffsets(obj,e){
+    //Copy Function
+    function copyToClipboard(e) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(e).select();
+        document.execCommand("copy");
+        $temp.remove();
+    }
+
+    function copySuccess(e){
+        e.append('<span style="font-size:20px;margin:30% auto 0 auto;color:white;display:block;letter-spacing:1px;">COPIED</span>');
+        e.css('background-color', 'rgba(255,255,0,.5)');
+        e.animate({opacity:0} , 300,function(){
+            e.remove();
+        })
+    }
+
+    //Calulate Positions
+    function calcOffsets(obj,e){
         var offset = obj.offset();
 
         e.preventDefault(); 
@@ -17,23 +34,24 @@ $.fn.absCruncher = function() {
         var offsetTop = (toppos / height * 100).toFixed(1);
 
         return {l:offsetLeft,t:offsetTop}
-	};
+    };
 
-	//Prevent click function
-	$('a' , this).on('click',function(e){
+    //Prevent click function
+    $('a' , this).on('click',function(e){
         e.preventDefault(); 
-	});
+    });
 
-	//Calculate first point
+    //Calculate first point
     this.mousedown(function(e){
         var calcOutput = calcOffsets($(this),e)
-        console.log(event.type)
+        e.stopPropagation();
         $(this).attr({'data-top':calcOutput.t,'data-left':calcOutput.l})
     });
 
     //Calculate second point, determine width and height, and 
     this.mouseup(function(e){
         var calcOutput = calcOffsets($(this),e)
+        e.stopPropagation();
 
         //Calculate First Point
         var originalTop = $(this).attr('data-top');
@@ -43,13 +61,19 @@ $.fn.absCruncher = function() {
         var cssWidth = (calcOutput.l - originalLeft).toFixed(1);
         var cssHeight = (calcOutput.t - originalTop).toFixed(1);
 
-        //Make Box
-        $(this).append('<a class="setPos" style="top: '+originalTop+'%; left: '+originalLeft+'%; width:' +cssWidth+ '%; height:' +cssHeight+ '%;"></a>')
+
+        if(!(cssWidth == 0)){
+            //Make Box
+            $(this).append('<a class="setPos" style="top: '+originalTop+'%; left: '+originalLeft+'%; width:' +cssWidth+ '%; height:' +cssHeight+ '%;"></a>');
+    
+        }
     });
 
     //Click on a box to get position info to copy paste
-	$(document).on('click','.setPos', function(e){
+    $(document).on('click','.setPos', function(e){
         e.preventDefault(); 
-        window.prompt("Copy to clipboard: Ctrl+C, Enter", $(this).attr('style'));
-	});
+        copyToClipboard($(this).attr('style'))
+        copySuccess($(this))
+        //window.prompt("Copy to clipboard: Ctrl+C, Enter", $(this).attr('style'));
+    });
 };
